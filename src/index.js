aa123ba01b21a207c88a90874cce922688bf13d8
Ashley Telsan <ash@telsanstudios.com>
2022-04-02 16:00:20 -0400

    A Simple Mutation
    - adds a mutation to the GraphQL API

--- src/index.js
+++ src/index.js
@@ -1,41 +1,42 @@
 const { ApolloServer } = require ('apollo-server');
+const fs = require('fs');
+const path = require('path');
 
-// 1 
-const typeDefs = `
-  type Query {
-    info: String!
-    feed: [Link!]!
-  }
 
-  type Link{
-    id: ID!
-    description: String!
-    url: String!
-  }
-`
 let links = [{
   id: 'link-0',
   url: 'www.howtographql.com',
   description: 'Fullstack tutorial for GraphQL'
 }]
 
-// 2
+
 const resolvers = {
   Query: {
     info: () => 'This is the API of a Hackernews Clone',
     feed: () => links,
   }, 
 
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
-  }
+  Mutation: {
+    post: (parent, args) => {
+
+      let idCount = links.length
+
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    }
+  },
 }
 
-// 3
+
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(
+    path.join(__dirname, 'schema.graphql'), 'utf-8'
+  ),
   resolvers,
 })
 
